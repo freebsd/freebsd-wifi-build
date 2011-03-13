6e399e52bccfe802f8f8ca1ad109ec59e25c0740
adrian.chadd <adrian.chadd@82540c0a-c834-3e38-1392-bf40f5692bc6>
2011-03-13 00:48:17 +0000

    Begin fleshing out a unified set of configuration variables which all build scripts will use.
    This will simplify the build process!
    
    
    
    git-svn-id: https://freebsd-wifi-build.googlecode.com/svn/trunk@5 82540c0a-c834-3e38-1392-bf40f5692bc6

---
+++ build/lib/cfg.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# This library takes a set of configuration parameters
+# and builds the configuration variables used elsewhere
+# in the build system.
+
+# Configuration variables which need to be set
+# CFGNAME
+# BUILDNAME
+# UBNT_ARCH
+# UBNT_VERSION
+# KERNCONF
+# TARGET
+# TARGET_ARCH
+# TARGET_CPUTYPE
+# BUILD_FLAGS
+# LOCAL_DIRS
+
+# X_MAKEFS_ENDIAN - what endian-ness should the makefs FFS be?
+X_MAKEFS_ENDIAN="be"
+
+# X_MAKEFS_FLAGS - what flags to pass to makefs when building the MFS image
+X_MAKEFS_FLAGS="version=1,bsize=4096,fsize=512"
+
+# X_FSSIZE - how big to make the makefs?
+X_FSSIZE="10240"
+
+# Variables defined by this script
+
+# X_MAKEOBJDIRPREFIX - where to place object files
+X_MAKEOBJDIRPREFIX="${CUR_DIR}/../obj/${BUILDNAME}/" ; export X_MAKEOBJDIRPREFIX
+
+# X_DESTDIR - where to place the install root 
+X_DESTDIR="${CUR_DIR}/../root/${BUILDNAME}"
+
+# Variables that need to be defined by one of the build scripts
+# but I haven't decided whether they're configured by the user
+# or automatically generated.
+
+# X_FSIMAGE
+X_FSIMAGE="/tmp/mfs.img"
+
+# X_STAGING_FSROOT
+X_STAGING_FSROOT="/tmp/fs"
+
+# X_KERNEL
+X_KERNEL="/tftpboot/kernel.${KERNCONF}"
+
+# Stuff that may be duplicates
+
+# X_DESTROOT
+
