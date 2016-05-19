freebsd/freebsd-wifi-build

commit 6194d0fa89ad4913a69ed51fb8ba2c72aee800bc

(feature) add ability to specify extra flags for makefs

build/lib/cfg.sh
@@ -50,7 +50,7 @@ X_KERNSUFFIX=${X_KERNSUFFIX:="${KERNCONF}"}
 X_MAKEFS_ENDIAN=${X_MAKEFS_ENDIAN:="be"}
 
 # X_MAKEFS_FLAGS - what flags to pass to makefs when building the MFS image
-X_MAKEFS_FLAGS="version=1,bsize=4096,fsize=512"
+X_MAKEFS_FLAGS="version=1,bsize=4096,fsize=512,${X_MAKEFS_FLAGS_EXT}"
 
 # X_MAKEFS_FULL_FLAGS - what flags to pass to makefs when building the full image
 X_MAKEFS_FULL_FLAGS="version=2"

commit f95ff9deb88c419968472787fbfebb9a9fb65cfd

Fix fsimage target
Commit 6194d0fa89ad4913a69ed51fb8ba2c72aee800bc broke fsimage
target for configs that do not specify X_MAKEFS_FLAGS_EXT (almost
all of the current configs), so fix that.

build/lib/cfg.sh
@@ -50,7 +50,10 @@ X_KERNSUFFIX=${X_KERNSUFFIX:="${KERNCONF}"}
 X_MAKEFS_ENDIAN=${X_MAKEFS_ENDIAN:="be"}
 
 # X_MAKEFS_FLAGS - what flags to pass to makefs when building the MFS image
-X_MAKEFS_FLAGS="version=1,bsize=4096,fsize=512,${X_MAKEFS_FLAGS_EXT}"
+X_MAKEFS_FLAGS="version=1,bsize=4096,fsize=512"
+if [ "x" != "x${X_MAKEFS_FLAGS_EXT}" ]; then
+	X_MAKEFS_FLAGS="${X_MAKEFS_FLAGS},${X_MAKEFS_FLAGS_EXT}"
+fi
 
 # X_MAKEFS_FULL_FLAGS - what flags to pass to makefs when building the full image
 X_MAKEFS_FULL_FLAGS="version=2"